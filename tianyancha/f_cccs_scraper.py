e172541c8870220a7db2aeb95c1cad72594f6676
Helixcs <Yuious12345>
2018-07-24 17:28:43 +0800

    add tianyancha

--- tianyancha/f_cccs_scraper.py
+++ tianyancha/f_cccs_scraper.py
@@ -3,24 +3,19 @@
 """
  Verion: 1.0
  Author: Helixcs
- Site: https://iliangqunru.bitcron.com/
  File: f_cccs_scraper.py
  Time: 7/24/18
 """
-import logging
-import sys
+import json
 import os
-import requests
 import time
-import lxml
-from lxml import html
-from cpca import *
 
-level = logging.DEBUG
-format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-datefmt = '%Y-%m-%d %H:%M'
-logging.basicConfig(level=level, format=format, datefmt=datefmt)
-logger = logging.getLogger(__name__)
+import lxml
+import requests
+from cpca import *
+from lxml import html
+
+_f_box = []  # fuck_box
 
 
 def cookies_string_to_dict(cookies_string):
@@ -45,7 +40,6 @@ def cookies_string_to_dict(cookies_string):
 
 # 天眼查cookies
 global_cookies = """
-aliyungf_tc=AQAAAKSZCyj/Pw4Aa9oKcG3TzFVEf18+; csrfToken=D7y9XKBbqKIV_Fa2Di5UKrlh; TYCID=f6de07308ede11e8a54aebc536b5d095; undefined=f6de07308ede11e8a54aebc536b5d095; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1532365213,1532365772,1532394893; _ga=GA1.2.1376886145.1532394894; _gid=GA1.2.1664725936.1532394894; ssuid=8800051336; token=5f8463c57cd64ebfb45fbe56475b6901; _utm=939c54f5cbaf4bd7acc821d1915ded0a; tyc-user-info=%257B%2522token%2522%253A%2522eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzUxNTEwNTU3MiIsImlhdCI6MTUzMjM5NDk0NCwiZXhwIjoxNTQ3OTQ2OTQ0fQ.efDRyIQPrXyDVRMdrCUAug3lofayBfvZAYjXHLSxdSfNbpqVPX7eOmHT1NI53VOab7-Pn8EsDSIA9fveoC3U3w%2522%252C%2522integrity%2522%253A%25220%2525%2522%252C%2522state%2522%253A%25220%2522%252C%2522redPoint%2522%253A%25220%2522%252C%2522vipManager%2522%253A%25220%2522%252C%2522vnum%2522%253A%25220%2522%252C%2522onum%2522%253A%25220%2522%252C%2522mobile%2522%253A%252213515105572%2522%257D; auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzUxNTEwNTU3MiIsImlhdCI6MTUzMjM5NDk0NCwiZXhwIjoxNTQ3OTQ2OTQ0fQ.efDRyIQPrXyDVRMdrCUAug3lofayBfvZAYjXHLSxdSfNbpqVPX7eOmHT1NI53VOab7-Pn8EsDSIA9fveoC3U3w; Hm_lpvt_e92c8d65d92d534b0f
 """
 global_headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
@@ -61,17 +55,18 @@ global_headers = {
 }
 
 
-def fetch_remote_html(page: int = 1):
+def fetch_remote_html(page: int = 1) -> dict:
     uri = 'https://www.tianyancha.com/pagination/bid.xhtml?ps=30&pn=%s&id=2322751222' % page
     try:
         r = requests.get(url=uri, headers=global_headers, cookies=cookies_string_to_dict(global_cookies))
         time.sleep(6)
-        return {'status_code':r.status_code,'text':''} if r.status_code != 200 or '访问禁止' in r.text else {'status_code':r.status_code,'text':r.text}
+        return {'status_code': r.status_code, 'text': ''} if r.status_code != 200 or '访问禁止' in r.text else {
+            'status_code': r.status_code, 'text': r.text}
     except Exception as ex:
-        return {'status_code':500,'text':''}
+        return {'status_code': 500, 'text': ''}
 
 
-def fetch_remote_raw_link(uri_from: str)->str:
+def fetch_remote_raw_link(uri_from: str) -> str:
     try:
         time.sleep(6)
         r = requests.get(url=uri_from, headers=global_headers, cookies=cookies_string_to_dict(global_cookies))
@@ -88,17 +83,16 @@ def read_html_file(file_path: str) -> str:
         return fuck.read()
 
 
-def parser_data(html_data: str) -> list:
+def parser_data(html_data: str) -> None:
     """ parser construct data from html"""
     if html_data is None or html_data == '':
         return []
 
-    _f_box = []
     _h = lxml.html.fromstring(html=html_data)
     _x_list = _h.xpath("//tbody/tr/td")
     for i in range(0, len(_x_list), 4):
         publish_time = _x_list[i + 1].text
-        title = ''.join(_x_list[i + 2].xpath(".//a/text()"))
+        title = ''.join(_x_list[i + 2].xpath(".//a/text()")).replace("\"", "")
         title_link = ''.join(_x_list[i + 2].xpath(".//a/@href"))
         raw_link = fetch_remote_raw_link(title_link)
         location = _x_list[i + 3].text
@@ -107,28 +101,64 @@ def parser_data(html_data: str) -> list:
         province = df.ix[[0]].values[0][0]
         city = df.ix[[0]].values[0][1]
         county = df.ix[[0]].values[0][2]
+        province = ""
+        city = ""
+        county = ""
         _f_box.append(
             {'province': province, 'city': city, 'county': county, 'publish_time': publish_time, 'title': title,
              'title_link': title_link, 'raw_link': raw_link, 'location': location})
-    return _f_box
 
 
-def main():
+def main_scraper():
     tmp_counter = 1
     while True:
         code_mapping_text = fetch_remote_html(tmp_counter)
         code = code_mapping_text.get("status_code")
         html_text = code_mapping_text.get('text')
         # 正常返回且返回结果为空，表示已经到达最大页数
-        if code == 200 and html_text =='':
+        if code == 200 and html_text == '':
             break
         # 实际上一共只有5页
-        if tmp_counter >5:
+        if tmp_counter > 5:
             break
         html_text = read_html_file('bid' + str(tmp_counter) + '.htm') if html_text == '' else html_text
-        fuck_box = parser_data(html_text)
+        parser_data(html_text)
+        tmp_counter += 1
 
-        tmp_counter+=1
+    print(_f_box)
+
+
+# ------------------------ data has been handle and forget above --------------------
+# 结构化后json数据文件
+
+TARGET_JSON = "structure_data.json"
+
+
+def generate_data_for_echarts():
+    if not os.path.exists('structure_data.json'):
+        return
+    cc_data = []
+    with open(TARGET_JSON, mode='r+', encoding='utf-8') as f_target_file:
+        json_map = json.load(fp=f_target_file)
+        for item in json_map:
+            if not item.get('county') == '':
+                location_name = item.get('county')
+            else:
+                if not item.get('city') == '':
+                    location_name = item.get('city')
+                else:
+                    location_name = item.get('province')
+            single_way = [{'name': '长春市'},
+                          {'name': location_name},
+                          {'name': item.get('title')},
+                          {'name': item.get('publish_time')},
+                          {'name': item.get('title_link')},
+                          {'name': item.get('raw_link')},
+                          {'name': item.get('location')}]
+            cc_data.append(single_way)
+    print(cc_data)
+
 
 if __name__ == '__main__':
-    main()
+    # main_scraper()
+    generate_data_for_echarts()
